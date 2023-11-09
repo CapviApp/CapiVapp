c36b310c0794c160c62327510fd6137b7d273584
Thais Florentin <84157862+LennyBla@users.noreply.github.com>
2023-11-09 13:15:31 -0300

    Update NewOS.jsx

--- src/screens/Inside/NewOS.jsx
+++ src/screens/Inside/NewOS.jsx
@@ -1,106 +1,212 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native'; // Importe FlatList
-import { doc, setDoc, collection, updateDoc, deleteDoc, query, where, getDocs } from "firebase/firestore";
-import firebase, { db } from '../../config/firebase';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import {addDoc, collection,query, getDocs,updateDoc,deleteDoc,where,} from 'firebase/firestore';
+import { db } from '../../config/firebase';
+import {format,addHours,set,getUnixTime,} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz/esm';
 
 export default function NewOS() {
-  const [username, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [users, setUsers] = useState([]);
+  const [osId, setOsId] = useState(''); // Adicione o estado para o ID da OS
+
+  const [cliente, setCliente] = useState('');
+  const [tipoHardware, setTipoHardware] = useState('');
+  const [tipoServico, setTipoServico] = useState('');
+  const [outros, setOutros] = useState('');
+  const [prioridade, setPrioridade] = useState('baixa');
+  const [comentario, setComentario] = useState('');
+  const [descricaoProduto, setDescricaoProduto] = useState('');
+  const [status, setStatus] = useState('Novo'); // Corrija para definir um valor inicial
   const [editMode, setEditMode] = useState(false);
+  const [osList, setOSList] = useState([]);
 
-  const userCollectionRef = collection(db, 'teste');
+  const osCollectionRef = collection(db, 'teste');
 
-  const adicionar = async () => {
+  const getNextOsId = async () => {
     try {
-      await setDoc(doc(userCollectionRef, email), {
-        nome: username,
-        email: email,
-      });
-      console.log('Cadastro realizado com sucesso!');
-      limparCampos();
-      loadUsers(); // Atualize a lista de usuários após o cadastro
+      const q = query(osCollectionRef);
+      const querySnapshot = await getDocs(q);
+      const osCount = querySnapshot.size;
+      const nextId = osCount + 1;//aqui ele ta fazendo a contagem
+      setOsId(nextId.toString());
+      //console.log('osId definido:', osId); // sla parece q ele substitui ou aparece tanto o id quando a contagem desse satanas
     } catch (error) {
-      console.error('Erro ao cadastrar:', error);
+      console.error('Erro ao obter o próximo ID da Ordem de Serviço:', error);
+    }
+  };
+
+  useEffect(() => {
+    getNextOsId();
+  }, []);
+
+  const adicionarOS = async () => {
+    try {
+      const dataAtualUTC = new Date();
+      const dataAtualBrasilia = utcToZonedTime(dataAtualUTC, 'America/Sao_Paulo');
+      const dataFormatada = format(dataAtualBrasilia, 'dd-MM-yyyy / HH:mm');//yyyy-MM-dd HH:mm:ssXXX'
+  
+      const docRef = await addDoc(osCollectionRef, {
+        data: dataFormatada,
+        cliente: cliente,
+        tipoHardware: tipoHardware,
+        tipoServico: tipoServico,
+        outros: outros,
+        prioridade: prioridade,
+        comentario: comentario,
+        descricaoProduto: descricaoProduto,
+        status: status,
+      });
+  
+      const osId = docRef.id;
+  
+      console.log('Ordem de serviço cadastrada com sucesso! ID da OS:', osId);
+      limparCampos();
+      loadOS();
+    } catch (error) {
+      console.error('Erro ao cadastrar ordem de serviço:', error);
     }
   }
+  
+  
 
-  const update = async () => {
+  const updateOS = async () => {
     try {
-      await updateDoc(doc(userCollectionRef, email), {
-        nome: username,
+      const osQuery = query(osCollectionRef, where('cliente', '==', cliente));
+      const osQuerySnapshot = await getDocs(osQuery);
+
+      osQuerySnapshot.forEach(async (doc) => {
+        await updateDoc(doc.ref, {
+          cliente: cliente,
+          tipoHardware: tipoHardware,
+          tipoServico: tipoServico,
+          outros: outros,
+          prioridade: prioridade,
+          comentario: comentario,
+          descricaoProduto: descricaoProduto,
+          status: status,
+        });
       });
+
       console.log('Atualização realizada com sucesso!');
       limparCampos();
       setEditMode(false);
-      loadUsers(); // Atualize a lista de usuários após a atualização
+      loadOS();
     } catch (error) {
       console.error('Erro ao atualizar:', error);
     }
   }
 
-  const deleteUser = async () => {
+  const deleteOS = async () => {
     try {
-      await deleteDoc(doc(userCollectionRef, email));
-      console.log('Usuário deletado com sucesso!');
+      const osQuery = query(osCollectionRef, where('cliente', '==', cliente));
+      const osQuerySnapshot = await getDocs(osQuery);
+
+      osQuerySnapshot.forEach(async (doc) => {
+        await deleteDoc(doc.ref);
+      });
+
+      console.log('Ordem de serviço excluída com sucesso!');
       limparCampos();
-      loadUsers(); // Atualize a lista de usuários após a exclusão
+      loadOS();
     } catch (error) {
-      console.error('Erro ao deletar:', error);
+      console.error('Erro ao excluir:', error);
     }
   }
 
-  const listUser = async () => {
+  const listOS = async () => {
     try {
-      const q = query(userCollectionRef, where('email', '==', email));
+      const q = query(osCollectionRef);
       const querySnapshot = await getDocs(q);
-      const userData = [];
+
+      const osData = [];
       querySnapshot.forEach((doc) => {
-        userData.push(doc.data());
+        osData.push({ id: doc.id, ...doc.data() });
       });
-      setUsers(userData);
+      setOSList(osData);
     } catch (error) {
       console.error('Erro ao listar:', error);
     }
   }
 
-  const loadUsers = async () => {
+  const loadOS = async () => {
     try {
-      const q = query(userCollectionRef);
+      const q = query(osCollectionRef);
       const querySnapshot = await getDocs(q);
-      const userData = [];
+      const osData = [];
       querySnapshot.forEach((doc) => {
-        userData.push(doc.data());
+        osData.push({ id: doc.id, ...doc.data() });
       });
-      setUsers(userData);
+      setOSList(osData);
     } catch (error) {
-      console.error('Erro ao carregar usuários:', error);
+      console.error('Erro ao carregar ordens de serviço:', error);
     }
   }
 
   const limparCampos = () => {
-    setName('');
-    setEmail('');
+    setCliente('');
+    setTipoHardware('');
+    setTipoServico('');
+    setOutros('');
+    setPrioridade('baixa');
+    setComentario('');
+    setDescricaoProduto('');
+    setStatus('Novo');
   }
 
   useEffect(() => {
-    loadUsers(); // Carregue a lista de usuários ao iniciar o componente
+    loadOS();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>NewOS</Text>
-      <TextInput placeholder='Nome' onChangeText={(value) => setName(value)} value={username} />
-      <TextInput placeholder='Email' onChangeText={(value) => setEmail(value)} value={email} />
+      <Text style={styles.title}>Nova Ordem de Serviço (OS)</Text>
+
+      <Text>ID da OS:</Text>{/**aqui ele exibe a contagem no começo, sempremicao de edicao */}
+      <Text>{osId}</Text>
+
+      <Text>Cliente:</Text>
+      <TextInput placeholder="Selecione um cliente ou digite um novo" value={cliente} onChangeText={text => setCliente(text)} />
+
+      <Text>Tipo de Hardware:</Text>
+      <Picker selectedValue={tipoHardware} onValueChange={itemValue => setTipoHardware(itemValue)}>
+        <Picker.Item label="Selecione" value="" />
+        <Picker.Item label="Opção 1" value="Opção 1" />
+        <Picker.Item label="Opção 2" value="Opção 2" />
+      </Picker>
+
+      <Text>Tipo de Serviço:</Text>
+      <Picker selectedValue={tipoServico} onValueChange={itemValue => setTipoServico(itemValue)}>
+        <Picker.Item label="Selecione" value="" />
+        <Picker.Item label="Opção 1" value="Opção 1" />
+        <Picker.Item label="Opção 2" value="Opção 2" />
+      </Picker>
+
+      <Text>Outros:</Text>
+      <TextInput placeholder="Comentários adicionais" value={outros} onChangeText={text => setOutros(text)} />
+
+      <Text>Prioridade:</Text>
+      <Picker selectedValue={prioridade} onValueChange={itemValue => setPrioridade(itemValue)}>
+        <Picker.Item label="Baixa" value="baixa" />
+        <Picker.Item label="Média" value="média" />
+        <Picker.Item label="Alta" value="alta" />
+      </Picker>
+
+      <Text>Comentário:</Text>
+      <TextInput placeholder="Comentário da OS" value={comentario} onChangeText={text => setComentario(text)} />
+
+      <Text>Descrição do Produto:</Text>
+      <TextInput placeholder="Descrição do produto" value={descricaoProduto} onChangeText={text => setDescricaoProduto(text)} />
+
+      <Text>Status:</Text>
+      <Picker selectedValue={status} onValueChange={itemValue => setStatus(itemValue)} />
 
       {editMode ? (
-        <Button onPress={update} title='Atualizar' />
+        <Button onPress={updateOS} title='Atualizar' />
       ) : (
-        <Button onPress={adicionar} title='Salvar' />
+        <Button onPress={adicionarOS} title='Salvar' />
       )}
 
-      <Button onPress={deleteUser} title='Deletar' />
+      <Button onPress={deleteOS} title='Deletar' />
 
       {editMode ? (
         <Button onPress={() => { limparCampos(); setEditMode(false); }} title='Cancelar' />
@@ -108,34 +214,43 @@ export default function NewOS() {
         <Button onPress={() => setEditMode(true)} title='Editar' />
       )}
 
-      <Button onPress={loadUsers} title='Listar' />
+      <Button onPress={listOS} title='Listar' />
 
-
-      <Text style={styles.listTitle}>Lista de Usuários:</Text>
+      <Text style={styles.listTitle}>Lista de Ordens de Serviço:</Text>
+      {/**O id da OS aparece com 'undefined para vizualizacao */}
       <FlatList
-        data={users}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Text>{`Nome: ${item.nome}, Email: ${item.email}`}</Text>
-        )}
-      />
+      data={osList}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item }) => (
+        <Text>{`ID da OS: ${item.id}
+        Data: ${item.data}
+        Cliente: ${item.cliente}
+        TipoHardware: ${item.tipoHardware}
+        TipoServico: ${item.tipoServico} 
+        Outros: ${item.outros} 
+        Prioridade: ${item.prioridade}
+        Comentario: ${item.comentario}
+        DescricaoProduto: ${item.descricaoProduto}
+        Status: ${item.status}`}</Text>
+      )}
+/>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
   },
   title: {
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  listTitle: {
     fontSize: 18,
     fontWeight: 'bold',
   },
-});
+  listTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+};
