4fbeeafe9687333035aee91ac7831ca6850ebbd3
LaraVic9 <laravicspereira@gmail.com>
2023-11-16 11:36:33 -0300

    subindo mudancas tab

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,52 +1,68 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { AntDesign, Feather } from '@expo/vector-icons'
-import { View, Text, Platform } from "react-native";
 import { View, Text, Platform, StyleSheet } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Home, New, Profile, Historico, Cliente } from '../screens/Inside';
 import COLORS from '../constants/color'
 
+import CustumTabBarBotton from '../components/layout/CustomTabBarButton/CustumTabBarBotton';
+
 const Tab = createBottomTabNavigator()
 
-const screenOptions = {
-    tabBarShowLabel: false,
-    headerShown: false,
-    tabBarHideOnKeyboard: true,
-    tabBarStyle: {
-      position: "absolute",
-      bottom: 0,
-      right: 0,
-      left: 0,
-      elevation: 0,
-      height: 60,
-      backgroundColor: COLORS.white,
-      headerShown: false 
-    },
-  };
 
 export default function TabRoutes(){
     return (
-        <Tab.Navigator screenOptions={screenOptions}>
-           <Tab.Screen 
+        <Tab.Navigator screenOptions={(route) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarInactiveTintColor: COLORS.dark,
+          tabBarStyle: styles.tabBarStyle,
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarIcon: ({color, size, focused}) => {
+            let iconName;
+
+            if (route.name === Home) {
+              iconName = focused ? 'ios-home-sharp' : 'ios-home-outline';
+            } else if (route.name === Cliente) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            } else if (route.name === New) {
+              iconName = focused ? 'wallet' : 'wallet-outline';
+            } else if (route.name === Profile) {
+              iconName = focused ? 'wallet' : 'wallet-outline';
+            }else if (route.name === Historico) {
+              iconName = focused
+                ? 'md-notifications-sharp'
+                : 'md-notifications-outline';
+            }
+
+            return <Icon name={iconName} size={22} color='black' />;
+          }
+        })}
+         
+          
+          >
+            <Tab.Screen 
                 name='Home'
                 style={styles.tabButton}
                 component={Home}
                 options={{
                   tabBarButton: props => <CustumTabBarBotton route="home" {...props} />,
-
+                    
                 }}
             />
-           <Tab.Screen 
+            <Tab.Screen 
                 name='cliente'
                 component={Cliente}
                 options={{
                   tabBarButton: props => <CustumTabBarBotton {...props} />,
+                   
                 }}
             />
             <Tab.Screen 
                 name='new'
                 component={New}
                 options={{
+                  tabBarButton: props => <CustumTabBarBotton {...props} />,
                     tabBarIcon: ({ focused }) => {
                         return (
                             <View
@@ -62,7 +78,7 @@ export default function TabRoutes(){
                               borderColor: COLORS.white,
                             }}
                           >
-                            <Fontisto name="plus-a" size={24} color={COLORS.white} />
+                           
                           </View>
                         );
                       },
@@ -73,7 +89,7 @@ export default function TabRoutes(){
                 component={Profile}
                 options={{
                   tabBarButton: props => <CustumTabBarBotton {...props} />,
-
+                    
                 }}
             />
             <Tab.Screen 
@@ -81,10 +97,22 @@ export default function TabRoutes(){
                 component={Historico}
                 options={{
                   tabBarButton: props => <CustumTabBarBotton {...props} />,
-
+                   
                 }}
             />
 
         </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create ({
+  tabBarStyle: {
+    position: 'absolute',
+    backgroundColor: COLORS.transparent,
+    borderTopWidth: 0,
+    bottom: 15,
+    right: 10,
+    left: 10,
+    height: 92,
+  },
+})
