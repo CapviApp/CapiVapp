c724b2ddfe6f259b361033dc4b7416321ac47e7f
LaraVic9 <laravicspereira@gmail.com>
2023-11-30 12:43:11 -0300

    subindo alteracoes NewCliente e NewOS

--- src/screens/Inside/NewCliente.jsx
+++ src/screens/Inside/NewCliente.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, SectionList } from 'react-native';
 import { doc, setDoc, collection, updateDoc, deleteDoc, getDocs, getDoc } from "firebase/firestore";
 import firebase, { db } from '../../config/firebase';
-
+import { Button } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Cliente() {
   const [username, setUsername] = useState('');
@@ -83,33 +84,65 @@ export default function Cliente() {
     }
   }
 
-  return (
-    <View>
-      <Text>Novos Cliente</Text>
-      <TextInput placeholder="Nome:" onChangeText={(value) => setUsername(value)} />
-      <TextInput placeholder="Email:" onChangeText={(value) => setEmail(value)} />
-      <TextInput placeholder="CNPJ/CPF:" onChangeText={(value) => setCpfCnpj(value)} />
-      <TextInput placeholder="Telefone:" onChangeText={(value) => setTelefone(value)} />
-      <TextInput placeholder="Endereço:" onChangeText={(value) => setEndereco(value)} />
-      <Button onPress={adicionar} title="Novo" />
-      <Button onPress={update} title="Editar" />
-      <Button onPress={deleteUser} title="Excluir" />
-      <Button onPress={listUser} title="Listar" />
+  const data = [
+    { title: 'Seção 1', data: [/* ...itens da seção 1... */] },
+   
+  ];
 
-      {users.map((user, index) => (
-        <View key={index}>
-          <Text>Nome: {user.nome}</Text>
-          <Text>Email: {user.email}</Text>
-          <Text>CNPJ/CPF: {user.cnpj || user.cpf}</Text>
-          <Text>Número de Telefone: {user.telefone}</Text>
-          <Text>Endereço: {user.endereco}</Text>
-        </View>
-      ))}
+  // Função para renderizar um item individual na SectionList
+  const renderItem = ({ item }) => (
+    <View>
+      {/* Seu componente de item da lista (OsItemH) */}
+      <Text>{item}</Text>
     </View>
+  );
+
+  return (
+    <LinearGradient colors={['#08354a', '#10456e', '#08354a']} style={styles.backgroundColor}>
+     
+    <SectionList
+      sections={data}
+      renderItem={renderItem}
+      renderSectionHeader={({ section: { title } }) => (
+       
+        <View>
+          <View style={styles.inputContainer}>
+          <Text style={styles.title}>Novos Cliente</Text>
+            <TextInput placeholder="Nome:" onChangeText={(value) => setUsername(value)} style={styles.input} placeholderTextColor={color='white'}/>
+            <TextInput placeholder="Email:" onChangeText={(value) => setEmail(value)} style={styles.input} placeholderTextColor={color='white'}/>
+            <TextInput placeholder="CNPJ/CPF:" onChangeText={(value) => setCpfCnpj(value)} style={styles.input} placeholderTextColor={color='white'}/>
+            <TextInput placeholder="Telefone:" onChangeText={(value) => setTelefone(value)} style={styles.input} placeholderTextColor={color='white'}/>
+            <TextInput placeholder="Endereço:" onChangeText={(value) => setEndereco(value)} style={styles.input} placeholderTextColor={color='white'}/>
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button onPress={adicionar} mode='contained' style={styles.button}>Salvar</Button>
+            <Button onPress={update} mode='contained' style={styles.button}>Atualizar</Button>
+            <Button onPress={deleteUser} mode='contained' style={styles.button}>Deletar</Button>
+            <Button onPress={listUser} mode='contained' style={styles.button}>Listar</Button>
+          </View>
+          {users.map((user, index) => (
+            <View key={index}>
+              <Text>Nome: {user.nome}</Text>
+              <Text>Email: {user.email}</Text>
+              <Text>CNPJ/CPF: {user.cnpj || user.cpf}</Text>
+              <Text>Número de Telefone: {user.telefone}</Text>
+              <Text>Endereço: {user.endereco}</Text>
+            </View>
+          ))}
+        </View>
+      
+      )}
+      keyExtractor={(item, index) => index.toString()}
+    />
+   </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundColor: {
+    flex: 1,
+    width: '100%',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -119,5 +152,25 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 22,
     fontWeight: 'bold',
+    color: 'white',
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  button:{
+    marginBottom: 10,
+   
+  },
+  buttonContainer: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  input:{
+    borderBottomWidth: 1,
+    borderBottomColor: 'white',
+    paddingVertical: 5,
+  },
+  inputContainer: {
+    paddingHorizontal: 10,
   }
 });
